a3f924fc83cf532e97dbe21a3c1acf54ca14eb58
Puzzl3d3d <olivernorval08@outlook.com>
2023-11-17 00:39:28 +0000

    linux compatibility

--- cardRenderer.py
+++ cardRenderer.py
@@ -1,6 +1,7 @@
-
 import os
-os.system("color")
+
+os.system('' if os.name == 'nt' else 'color')
+os.system('cls' if os.name == 'nt' else 'clear')
 
 card = """
 **********
@@ -18,28 +19,31 @@ suitColours = {
     "Clubs": "{}",
     "Spades": "{}"
 }
-suitEmojis = {
-    "Hearts": "♥",
-    "Clubs": "♣",
-    "Diamonds": "♦",
-    "Spades": "♠"
-}
+suitEmojis = {"Hearts": "♥", "Clubs": "♣", "Diamonds": "♦", "Spades": "♠"}
 
-def combineStrings(str1, str2, *args, spacing="  ", suit1="Spades", suit2="Spades"):
-    combined = ""
-    str1 = str1.split("\n")
-    str2 = str2.split("\n")
-    
-    for i in range(min(len(str1), len(str2))):
-        combined += f"{suitColours[suit1].format(str1[i])}{spacing}{suitColours[suit2].format(str2[i])}\n"
-    return combined
+
+def combineStrings(str1,
+                   str2,
+                   *args,
+                   spacing="  ",
+                   suit1="Spades",
+                   suit2="Spades"):
+  combined = ""
+  str1 = str1.split("\n")
+  str2 = str2.split("\n")
+
+  for i in range(min(len(str1), len(str2))):
+    combined += f"{suitColours[suit1].format(str1[i])}{spacing}{suitColours[suit2].format(str2[i])}\n"
+  return combined
+
 
 def getCardDisplay(suit, value):
-    if suit is None or value is None:
-        return card.format(" "," "," "," ")
-    if value >= 10:
-        value = {10: "T", 11: "A", 12: "J", 13: "Q", 14: "K"}[value]
-    else:
-        value = str(value)
-    suitEmoji = suitEmojis[suit]
-    return suitColours[suit].format(card.format(value, suitEmoji, suitEmoji, value))
+  if suit is None or value is None:
+    return card.format(" ", " ", " ", " ")
+  if value >= 10:
+    value = {10: "T", 11: "A", 12: "J", 13: "Q", 14: "K"}[value]
+  else:
+    value = str(value)
+  suitEmoji = suitEmojis[suit]
+  return suitColours[suit].format(
+      card.format(value, suitEmoji, suitEmoji, value))
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ dealerCards = []
 
 import os
 def clear():
-    os.system("cls")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def getResult():
